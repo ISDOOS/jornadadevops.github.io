2f1d54684c90bb497ed3d526565671eae70e7c6f
Iago do Ó <142062407+ISDOOS@users.noreply.github.com>
2024-11-25 21:02:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-window.addEventListener("scroll", function() {
-    const backToTopButton = document.querySelector(".back-to-top");
-    if (window.scrollY > 500) {
-        backToTopButton.style.display = "block";
-    } else {
-        backToTopButton.style.display = "none";
-    }
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    const backToTopButton = document.querySelector('.back-to-top');
+
+    window.addEventListener('scroll', function () {
+        if (window.scrollY > 300) {
+            backToTopButton.style.display = 'block'; // Mostra o botão
+        } else {
+            backToTopButton.style.display = 'none'; // Oculta o botão
+        }
+    });
 });
